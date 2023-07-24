23761ce7104355ef8e3278e0555f0915dc25535e
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-24 17:59:45 +0100

    arrive in part 4 where i added the images

---
+++ flutter_application_1/lib/model/ad_banner.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+List<AdBanner> AdBannersListFromJson(String val) => List<AdBanner>.from(
+  json.decode(val)['data'].map((banner) => AdBanner.fromJson(banner))
+);
+class AdBanner{
+  final int id;
+  final String image;
+  final String populate;
+
+  AdBanner({
+    required this.id,
+    required this.image,
+    required this.populate,
+  });
+
+  factory AdBanner.fromJson(Map<String, dynamic> data) => AdBanner(
+    id:data['id'],
+    image: data['image'],
+    populate:data['populate'],
+  );
+}
