b62a49b912639be144cc754f581f61d9b76f6857
Asmae Mouradi <amouradi2002@gmail.com>
2023-07-24 13:07:08 +0100

    adding color in each page

--- flutter_application_1/lib/route/app_page.dart
+++ flutter_application_1/lib/route/app_page.dart
@@ -1,11 +1,17 @@
 
+import 'package:flutter_application_1/view/dashboard/dashboard_binding.dart';
+
 import '../view/dashboard/dashborad_screen.dart';
 import 'app_route.dart';
 import 'package:get/get.dart';
 
 class AppPage{
   static var list = [
-    GetPage(name: AppRoute.dashboard, page: () => const DashboardScreen())
+    GetPage(
+      name: AppRoute.dashboard, 
+      page: () => const DashboardScreen(),
+      binding: DashboardBinding()
+      )
   ];
   
 }
